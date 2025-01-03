a8d66cdf28623531f1f7764a285a1df419d56e30
mrzlanx532 <denis_danilov_97@mail.ru>
2025-01-03 20:07:58 +0300

    Init #2

--- docker/mysql/scripts/init.sql
+++ docker/mysql/scripts/init.sql
@@ -1,6 +1,6 @@
 SET GLOBAL time_zone = 'UTC';
-CREATE DATABASE IF NOT EXISTS `example_laravel_backend_for_nuxt` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'example_laravel_backend_for_nuxt'@'%' IDENTIFIED WITH mysql_native_password BY 'example_laravel_backend_for_nuxt';
-GRANT ALL ON *.* TO 'example_laravel_backend_for_nuxt'@'%';
+CREATE DATABASE IF NOT EXISTS `elb` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER 'elb'@'%' IDENTIFIED WITH mysql_native_password BY 'elb';
+GRANT ALL ON *.* TO 'elb'@'%';
 
-USE example_laravel_backend_for_nuxt;
+USE elb;
